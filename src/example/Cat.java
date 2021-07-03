cafda2ec5a4d218013227dcadef30ba9920d80ad
kim-shun <kimura1417080@gmail.com>
2021-07-03 11:05:40 +0900

    空腹具合を表現

--- src/example/Cat.java
+++ src/example/Cat.java
@@ -6,6 +6,8 @@ public class Cat {
 	
 	void eat() {
 		System.out.println(name + "> ご飯を食べるよ！おいしいにゃー");
+		System.out.println(name + "> お腹が一杯になったにゃー");
+		hungry = false;
 	}
 	
 	boolean isHungry() {
@@ -14,6 +16,8 @@ public class Cat {
 	
 	void playToy(String toy) {
 		System.out.println(name + "> " + toy + "で遊ぶよ。楽しいにゃー");
+		System.out.println(name + "> 遊んでお腹が減ったにゃー");
+		hungry = true;
 	}
 
 }
