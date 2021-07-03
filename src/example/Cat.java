fe56601cc66451f52c9f57a33f277a8316b91321
kim-shun <kimura1417080@gmail.com>
2021-07-03 09:43:37 +0900

    Initial commit

---
+++ src/example/Cat.java
@@ -0,0 +1,17 @@
+package example;
+
+public class Cat {
+	
+	void eat() {
+		System.out.println("ご飯を食べるよ！おいしいにゃー");
+	}
+	
+	boolean isHungry() {
+		return true;
+	}
+	
+	void playToy(String toy) {
+		System.out.println(toy + "で遊ぶよ。楽しいにゃー");
+	}
+
+}
---
+++ src/example/UseCatSample.java
@@ -0,0 +1,12 @@
+package example;
+
+public class UseCatSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Cat tama = new Cat();
+		tama.eat();
+		tama.playToy("ひも");
+	}
+
+}
