0486c6b91245ecb4f69cb1b5a7f94b50dbc7c14b
kim-shun <kimura1417080@gmail.com>
2021-07-05 10:56:53 +0900

    コンパイルエラー

--- src/example2/PublicAccessSample.java
+++ src/example2/PublicAccessSample.java
@@ -6,6 +6,11 @@ public class PublicAccessSample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Cat tama = new Cat();
+		tama.setName("タマ");
+		System.out.println(tama.getName());
+		tama.setAge(3);
+		System.out.println(tama.getAge());
 
 	}
 
