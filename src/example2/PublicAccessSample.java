821bbb5d2b26a0d946779a67d02983216a4578a4
kim-shun <kimura1417080@gmail.com>
2021-07-21 15:04:12 +0900

    privateなメソッドを定義

--- src/example2/PublicAccessSample.java
+++ src/example2/PublicAccessSample.java
@@ -8,9 +8,11 @@ public class PublicAccessSample {
 		// TODO 自動生成されたメソッド・スタブ
 		Cat tama = new Cat();
 		tama.setName("タマ");
-		System.out.println(tama.getName());
 		tama.setAge(3);
-		System.out.println(tama.getAge());
+		
+		tama.eat();
+		tama.playToy("ボール");
+		tama.introduceMyself();
 
 	}
 
