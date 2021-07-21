0117d73e6f6fa65f9be26ae228e8ba2b03f9464f
kim-shun <kimura1417080@gmail.com>
2021-07-21 15:15:02 +0900

    カプセル化されたクラスにアクセス

---
+++ src/example/EncapsulationSample.java
@@ -0,0 +1,12 @@
+package example;
+
+public class EncapsulationSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Cat tama = new Cat();
+		tama.setName("タマ");
+		System.out.println(tama.getName());
+	}
+
+}
